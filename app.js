84a7b1ae22ff8448699216c94fd7a38698a487ac
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-11 08:00:45 -0600

    Image upload and data.json completed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const data = require('./data.json');
+const data = require('./data.json');
 
 const app = express();
 
@@ -11,13 +11,13 @@ app.get('/', (req, res, next) => {
     res.render('index');
 })
 
-// app.get('/about', (req, res, next) => {
-//     res.render('about')
-// })
+app.get('/about', (req, res, next) => {
+    res.render('about')
+})
 
-// app.get('/projects/:id', (req, res, next) => {
+app.get('/projects/:id', (req, res, next) => {
 
-// })
+})
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
